be3f6c2cedfa09baed72cf1aff1c8a50b8256d5b
abdulbakisurajtest <surajabdulbaki19@gmail.com>
2021-02-19 22:55:25 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Undertaking form by Abdulbaki Suraj</title>
+	<style type="text/css">
+		input{
+			width: 60%;
+		}
+		*{
+			font-family: helvetica, arial, serif;
+		}
+	</style>
+</head>
+<body>
+	<form action="https://eportal.futminna.edu.ng/accpt/covid19.php" method="POST">
+		<h2>COVID19 UNDERTAKING FORM FOR FEDERAL UNIVERSITY OF TECHNOLOGY, MINNA</h2>
+		
+		<h3>Please fill the form below correctly</h3>
+		
+		<label>
+			<strong>Enter your full name</strong><br/><span style="color:red">fill in uppercase</span> (e.g LAST-NAME FIRST-NAME MIDDLE-NAME)<br/>
+			<input name="CandName" id="CandName" value="" required="required" placeholder="MUSA CHINEDU KUNLE">
+		</label><br/><br/>
+		
+		<label>
+			<strong>Enter your level</strong> (e.g 500)<br/>
+			<input type="number" name="level" id="level" value="" required="required">
+		</label><br/><br/>
+		
+		<label>
+			<strong>Enter your matriculation number</strong><br/><span style="color:red">fill in uppercase</span> (e.g 2021/1/55558AA)<br/>
+			<input name="MatNumber" id="MatNumber" value="" required="required">
+		</label><br/><br/>
+		
+		<label>
+			<strong>Enter your department</strong><br/><span style="color:red">fill first letter in uppercase</span> (e.g Mechanical Engineering)<br/>
+			<input name="deptname" id="deptname" value="" required="required">	
+		</label><br/><br/>
+
+		<p>
+			<strong>DISCLAIMER:</strong><br/>
+			This is an independent platform created by a fellow student.<br/>
+			It is meant as an assistive tool for students who do not have access to their school portal.<br/>
+			We do not claim any responsibility for any problem that might arise from the use of this platform.<br/>
+			Use at your own risk !!!<br/>
+		</p>
+		<button type="submit" >PRINT FORM</button>
+	</form>
+
+	<p>
+		Do you want to help other students?, click <a style="text-decoration: underline; color: rgb(29, 222, 161);" href="whatsapp://send?text=Kindly use the link below to fill your COVID19 UNDERTAKING FORM  http://abdulbakisuraj.xyz/covid19form"><strong>here to share on Whatsapp</strong></a>
+	</p>
+	<br/><br/><br/>
+	<!--<p>
+		You can reach out to me via twitter <a target="_blank" href="https://twitter.com/fliplikesuraj">@fliplikesuraj</a>
+	</p>-->
+	<p>
+		You can reach out to me via <a target="_blank" href="mailto:surajabdulbaki19@gmail.com">email</a>
+	</p>
+</body>
+</html>
